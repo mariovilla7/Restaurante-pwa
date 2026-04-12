ee068a063d5ca3b3e3d0ddaa2dacf3af9ec34ce4
Mario <villamario7@gmail.com>
2026-04-12 03:15:04 +0200

    fallo general

--- src/utils/deviceId.ts
+++ src/utils/deviceId.ts
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
-
 const DEVICE_ID_KEY = 'restaurant_device_id';
 
 /**
@@ -11,7 +9,7 @@ export function getDeviceId(): string {
   let deviceId = localStorage.getItem(DEVICE_ID_KEY);
 
   if (!deviceId) {
-    deviceId = uuidv4();
+    deviceId = crypto.randomUUID();
     localStorage.setItem(DEVICE_ID_KEY, deviceId);
   }
 
